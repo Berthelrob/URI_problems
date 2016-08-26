e5e4213422904e8b824745de5df77c224ffe4c9e
Robson Berthelsen <robson.brs@gmail.com>
2016-08-26 16:50:17 -0300

    Update 1110 - Throwing Cards Away.cpp

--- 1110 - Throwing Cards Away.cpp
+++ 1110 - Throwing Cards Away.cpp
@@ -1,35 +1,32 @@
 #include <iostream>
 #include <queue>
 
-
 using namespace std; 
 
 int main(void){
-	
-	
+
 	int a_cards, i, first_element;
 
-	
 	cin >> a_cards;
 	while(a_cards != 0){
 		
 		queue <int> r_queue_cards;
 		queue <int> d_queue_cards; 
-
+	
 		if(a_cards != 0 || a_cards <=50){
 			for(i = 1; i<=a_cards; i++){
 				r_queue_cards.push(i);
 			}
-
+	
 			while(r_queue_cards.size()!=1){
 				d_queue_cards.push(r_queue_cards.front());
 				r_queue_cards.pop();
-
+	
 				first_element = r_queue_cards.front();
 				r_queue_cards.pop();
 				r_queue_cards.push(first_element);
 			}
-			
+				
 			cout << "Discarded cards: " ;
 			while(d_queue_cards.size()!=0){
 				cout << d_queue_cards.front();
@@ -37,18 +34,17 @@ int main(void){
 				if(d_queue_cards.size() >=1)
 					cout << ", ";
 			}
-
+	
 			cout << endl;
-
 			cout << "Remaining card: ";
+	
 			while(r_queue_cards.size()!=0){	
 				cout << r_queue_cards.front() << endl;
 				r_queue_cards.pop();
 			}
 		}
-	cin >> a_cards;
+			cin >> a_cards;
 	}
-
 	return 0;
 }
 
